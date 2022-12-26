3e3b587f405925758566ba8c8d9575146feec806
Glassyflute <yuliya.bobir@gmail.com>
2022-12-26 17:49:26 +0600

    download and activate DRF for HW29

--- avito/urls.py
+++ avito/urls.py
@@ -22,6 +22,7 @@ from ads import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
     path('', views.root),
     path('ad/', include('ads.urls.ad')),
     path('cat/', include('ads.urls.cat')),
